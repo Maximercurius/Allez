0fc0606358d7d59c590bcc5c30df1896afc6ec95
Maxim <swiftdevfromsiberia@gmail.com>
2022-03-28 12:14:17 +0500

    DetailViewController added, labels and mapView covered with tests

---
+++ Allez/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Allez
+//
+//  Created by Makarov_Maxim on 28.03.2022.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+
+}
---
+++ AllezTests/Controllers/DetailViewControllerTests.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewControllerTests.swift
+//  AllezTests
+//
+//  Created by Makarov_Maxim on 28.03.2022.
+//
+
+import XCTest
+@testable import Allez
+
+class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: DetailViewController.self)) as? DetailViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testHasTitleLabel() {
+
+        
+        XCTAssertNotNil(sut.titleLabel)
+        XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDescriptionLabel() {
+        
+        XCTAssertNotNil(sut.descriptionLabel)
+        XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDateLabel() {
+        
+        XCTAssertNotNil(sut.dateLabel)
+        XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasMapView() {
+        
+        XCTAssertNotNil(sut.mapView)
+        XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
+
+}
