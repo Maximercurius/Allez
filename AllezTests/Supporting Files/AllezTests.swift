a18834f8b511ee2f47691889c2f7b72e1a459e40
Maxim <swiftdevfromsiberia@gmail.com>
2022-04-03 22:23:02 +0500

    NewTaskButton added and covered with Test

---
+++ AllezTests/Supporting Files/AllezTests.swift
@@ -0,0 +1,29 @@
+//
+//  AllezTests.swift
+//  AllezTests
+//
+//  Created by Makarov_Maxim on 03.04.2022.
+//
+
+import XCTest
+@testable import Allez
+
+
+class AllezTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    func testInitialViewControllerIsTaskListViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let rootViewController = navigationController.viewControllers.first as! TaskListViewController
+        
+        XCTAssertTrue(rootViewController is TaskListViewController)
+    }
+
+}
